8352df6b80ac559dba368c999783e3f1c6d7ee40
the-space-parakeet <spaceparakeet@proton.me>
2025-02-17 18:01:53 -0600

    Test NOT truth table

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -6,15 +6,20 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 
 TEST_CASE("Test AND truth table") {
-	REQUIRE(true && true == true);
-	REQUIRE(true && false == false);
-	REQUIRE(false && true == false);
-	REQUIRE(false && false == false);
+	REQUIRE((true && true) == true);
+	REQUIRE((true && false) == false);
+	REQUIRE((false && true) == false);
+	REQUIRE((false && false) == false);
 }
 
 TEST_CASE("Test OR truth table") {
-	REQUIRE(true || true == true);
-	REQUIRE(true || false == true);
-	REQUIRE(false || true == true);
-	REQUIRE(false || false == false);
+	REQUIRE((true || true) == true);
+	REQUIRE((true || false) == true);
+	REQUIRE((false || true) == true);
+	REQUIRE((false || false) == false);
+}
+
+TEST_CASE("Test NOT truth table") {
+	REQUIRE((! true) == false);
+	REQUIRE((! false) == true);
 }
